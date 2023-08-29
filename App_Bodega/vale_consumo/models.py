4c7af0006d3338ccfb6a1809fab8609a44557c83
Luis <cordillerainmobiliaria.redes@gmail.com>
2023-08-29 18:38:20 -0400

    feat: Versión 2.0.1
    Refactorización en el modelo de los recursos para poder hacer la importación masiva. Import-Export requiere un campo "id" como PK

--- App_Bodega/vale_consumo/models.py
+++ App_Bodega/vale_consumo/models.py
@@ -86,7 +86,7 @@ class Unidade(models.Model):
 
 # MODELO:RECURSO
 class Recurso(models.Model):
-    cod_recurso = models.CharField(primary_key=True, max_length=10)
+    cod_recurso = models.CharField(max_length=20)
     categoria_recurso = models.ForeignKey(Categoria, on_delete=models.CASCADE)
     nombre_recurso = models.CharField(max_length=100, blank=False, null=False)
     unidad = models.ForeignKey(Unidade, on_delete=models.CASCADE)
